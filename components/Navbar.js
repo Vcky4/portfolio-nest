5b9c9f7a893a16010e3aef05de2c774bdba78c54
Vcky4 <okon.victor.u@gmail.com>
2023-01-06 22:03:48 +0100

    nav responsiveness

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
 
+import { AiOutlineDownload } from 'react-icons/ai'
+import { CgMenuLeft } from 'react-icons/cg'
+
 const Navbar = () => {
     return (
         <div className='bg-white fixed top-0 w-[100%] z-10'>
-            <div className='container mx-auto flex justify-between items-center py-4'>
+            <div className='container mx-auto flex justify-between items-center py-4 px-4'>
                 <div className='text-[24px]'>Victor Ubong</div>
 
-                <div className='flex space-x-4'>
+                <div className='hidden md:flex space-x-4'>
                     <Link href='/'>
                         <div className='border border-transparent border-t- border-l-0 border-r-0
                          border-b-0 text-[18px] hover:border-[#ff4D41] transition duration-200'>Home</div>
@@ -31,8 +34,15 @@ const Navbar = () => {
                 </div>
 
                 <a href='#download'
-                    className='flex border border-[#ff4D41] px-4 py-1 text-[18px] text-[#ff4D41] 
-                    rounded-[5px] item-center gap-2 hover:bg-[#ff4D41] hover:text-white transition duration-200 '>Download CV</a>
+                    className='hidden md:flex border border-[#ff4D41] px-4 py-1 text-[18px] text-[#ff4D41] 
+                    rounded-[5px] item-center gap-2 hover:bg-[#ff4D41] hover:text-white transition duration-200 '>
+                    Download CV
+                    <AiOutlineDownload />
+                </a>
+
+                <div className='md:hidden text-[24px] '>
+                    <CgMenuLeft />
+                </div>
             </div>
         </div>
     )
